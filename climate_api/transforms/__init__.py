ac664aecc10a1d03e875962d425f3d2efd6ccaf9
Bjørn Sandvik <bjorn@dhis2.org>
2026-05-09 20:55:25 +0200

    refactor: remove deaccumulate_era5 transform from this PR

--- climate_api/transforms/__init__.py
+++ climate_api/transforms/__init__.py
@@ -7,8 +7,7 @@ Functions can be referenced by their dotted module path in the dataset YAML
 ``transforms`` list, the same way ``ingestion.function`` works.
 """
 
-from .deaccumulate import deaccumulate_era5
 from .reproject import reproject_to_instance_crs
 from .unit_conversion import convert_units
 
-__all__ = ["convert_units", "deaccumulate_era5", "reproject_to_instance_crs"]
+__all__ = ["convert_units", "reproject_to_instance_crs"]
--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,7 +1,7 @@
 import numpy as np
 import xarray as xr
 
-from climate_api.transforms import convert_units, deaccumulate_era5
+from climate_api.transforms import convert_units
 
 
 def _ds(varname: str, values: list[float], time_steps: int = 1) -> xr.Dataset:
@@ -38,25 +38,6 @@ class TestConvertUnits:
         ds = xr.Dataset({"t2m": xr.DataArray([300.0], attrs={"long_name": "temperature", "units": "K"})})
         result = convert_units(ds, {"variable": "t2m", "units": "kelvin", "convert_units": "degC"})
         assert result["t2m"].attrs["long_name"] == "temperature"
-
-
-class TestDeaccumulateEra5:
-    def test_differences_along_time(self):
-        ds = _ds("tp", [0.0, 1.0, 3.0, 6.0], time_steps=4)
-        result = deaccumulate_era5(ds, {"variable": "tp"})
-        assert result.sizes["time"] == 3
-        np.testing.assert_array_equal(result["tp"].values.flatten(), [1.0, 2.0, 3.0])
-
-    def test_clips_negative_values(self):
-        ds = _ds("tp", [3.0, 1.0, 4.0], time_steps=3)
-        result = deaccumulate_era5(ds, {"variable": "tp"})
-        assert (result["tp"].values >= 0).all()
-
-    def test_preserves_attrs(self):
-        data = np.array([[0.0], [1.0]])
-        ds = xr.Dataset({"tp": xr.DataArray(data, dims=["time", "x"], attrs={"units": "m"})})
-        result = deaccumulate_era5(ds, {"variable": "tp"})
-        assert result["tp"].attrs["units"] == "m"
 
 
 class TestRunTransformsPipeline:
